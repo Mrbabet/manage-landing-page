2bd75311db2cf23d1f18f81aaca8de5651f12ce5
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2022-08-25 09:19:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector(".burger-bars");
+const hiddenMenu = document.querySelector(".nav-mobile");
+
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("active");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("active");
+    menuOpen = false;
+  }
+});
+
+menuBtn.addEventListener("click", function () {
+  hiddenMenu.classList.toggle("active");
+});
